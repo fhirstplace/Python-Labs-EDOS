92030faf9c569491c139c4b1d5f8536018d25dd4
Liam Bye <liamcjbye@gmail.com>
2025-02-21 15:49:56 +0000

    added x-intercept threshold voltage finder

--- analysis.py
+++ analysis.py
@@ -43,11 +43,31 @@ for dataset in datasets:
         plt.errorbar(voltages,currents,xerr = voltage_std, yerr = current_std, color = (i/71, 0, 1 - i/71))
     i+=1
 
+#linear fit at steepest point to find threshold voltage
+for dataset in datasets:
+    currents = dataset[0][0]
+    current_std = dataset[0][1]
+    voltages = dataset[1][0]
+    voltage_std = dataset[1][1]
+    max_derivative = 0
+    max_derivative_index = 0
+    for i in range(len(currents) - 1):
+        gradient = (currents[i + 1] - currents[i]) / (voltages[i + 1] - voltages[i])
+        if gradient > max_derivative:
+            max_derivative = gradient
+            max_derivative_index = i
+    f = lambda x : max_derivative * (x - voltages[max_derivative_index]) + currents[max_derivative_index]
+    plt.errorbar(voltages, list(map(f,(voltages))),fmt="--", color = 'red')
+    print("x-intercept = " + str((-currents[max_derivative_index] / max_derivative) + voltages[max_derivative_index]))
+
+
 # make graph things
 title = "Id vs Vg for a MOSFET"
 plt.legend()
 plt.title(title)
 plt.ylabel('Id')
 plt.xlabel('Vg')
+plt.xlim(1.35, 2.85)
+plt.ylim(0, 0.00015)
 plt.savefig(fname = "Graphs/"+data_file[5:-4]+".png")
 plt.show()
