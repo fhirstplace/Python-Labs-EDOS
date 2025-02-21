e5ab75d1dcefba42ca046c753697a632944b73cd
Liam Bye <liamcjbye@gmail.com>
2025-02-21 16:52:01 +0000

    started (but not finished) cleaning up code

--- analysis.py
+++ analysis.py
@@ -5,30 +5,31 @@ import scipy.stats as stats
 
 #parse xml file
 data_file = 'Data/Ba133_uniradiated_linear.xml'
-tree = ET.parse(data_file) 
-root = tree.getroot()
-datasets = []
 
 #parse data
-for dataset in root:
-    currents = []
-    current_std = []
-    voltages = []
-    voltage_std = []
-    for entry in dataset:
-        if entry.tag == 'Current':
-            currents.append(float(entry[0].text))
-            current_std.append(float(entry[1].text))
-        elif entry.tag == 'Voltage':
-            voltages.append(float(entry[0].text))
-            voltage_std.append(float(entry[1].text))
-    item = [[currents, current_std], [voltages, voltage_std]]
-    datasets.append(item)
+#input: xml file
+#output: array of datasets, each dataset containing two arrays of currents (means ([0][0]) and stds ([0][1])) and voltages (means ([1][0]) and stds ([1][1]))
+def xml_to_datasets(data_file):
+    tree = ET.parse(data_file)
+    root = tree.getroot()
+    datasets = []
+    for dataset in root:
+        currents = []
+        current_std = []
+        voltages = []
+        voltage_std = []
+        for entry in dataset:
+            if entry.tag == 'Current':
+                currents.append(float(entry[0].text))
+                current_std.append(float(entry[1].text))
+            elif entry.tag == 'Voltage':
+                voltages.append(float(entry[0].text))
+                voltage_std.append(float(entry[1].text))
+        item = [[currents, current_std], [voltages, voltage_std]]
+        datasets.append(item)
+    return datasets
 
-
-times = range(0,len(root))
-# z = np.polyfit(voltages, currents, 1)
-# Y1p = np.poly1d(z)
+datasets = xml_to_datasets(data_file)
 
 #plot data
 i = 0
@@ -38,9 +39,9 @@ for dataset in datasets:
     voltages = dataset[1][0]
     voltage_std = dataset[1][1]
     if i % 10 == 0:
-        plt.errorbar(voltages,currents,xerr = voltage_std, yerr = current_std, label = "uniradiated", color = (i/71, 0, 1 - i/71))
+        plt.errorbar(voltages,currents,xerr = voltage_std, yerr = current_std, label = "uniradiated", color = (i/len(voltages), 0, 1 - i/len(voltages)))
     else:
-        plt.errorbar(voltages,currents,xerr = voltage_std, yerr = current_std, color = (i/71, 0, 1 - i/71))
+        plt.errorbar(voltages,currents,xerr = voltage_std, yerr = current_std, color = (i/len(voltages), 0, 1 - i/len(voltages)))
     i+=1
 
 #linear fit at steepest point to find threshold voltage
@@ -59,6 +60,7 @@ for dataset in datasets:
             gradient_err = gradient * np.sqrt((current_std[i] / currents[i])**2 + (voltage_std[i + 1] / voltages[i + 1])**2 + (voltage_std[i] / voltages[i])**2)
     f = lambda x : max_derivative * (x - voltages[max_derivative_index]) + currents[max_derivative_index]
     plt.errorbar(voltages, list(map(f,(voltages))),fmt="--", color = 'red')
+
     #error calculations
     x_intercept = (-currents[max_derivative_index] / max_derivative) + voltages[max_derivative_index]
     error = np.sqrt((currents[max_derivative_index] / max_derivative) * np.sqrt((current_std[max_derivative_index] / currents[max_derivative_index])**2 + (gradient_err / max_derivative)**2)**2 + voltage_std[max_derivative_index]**2)
@@ -70,7 +72,7 @@ plt.legend()
 plt.title(title)
 plt.ylabel('Id')
 plt.xlabel('Vg')
-plt.xlim(1.35, 2.85)
-plt.ylim(0, 0.00015)
+#plt.xlim(1.35, 2.85)
+#plt.ylim(0, 0.00015)
 plt.savefig(fname = "Graphs/"+data_file[5:-4]+".png")
 plt.show()
