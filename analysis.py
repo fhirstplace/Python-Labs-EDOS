57b655576efbbd60efec130a2ae289a770bcbc02
Liam Bye <liamcjbye@gmail.com>
2025-02-28 16:12:17 +0000

    added residuals

--- analysis.py
+++ analysis.py
@@ -3,6 +3,9 @@ import numpy as np
 import matplotlib.pyplot as plt
 import scipy.stats as stats
 
+fig, ax = plt.subplots(2, 1, sharex=True, figsize=(10,8), gridspec_kw={'height_ratios': [4, 1]})
+fig.subplots_adjust(hspace=0)
+
 iv_graph = False
 steepest_line = False
 derivative = False
@@ -48,10 +51,10 @@ def plot_iv(datasets):
         voltages = dataset[1][0]
         voltage_std = dataset[1][1]
         if i % 10 == 0:
-            plt.errorbar(voltages,currents,xerr = voltage_std, yerr = current_std, label = str("t="+str(i)), color = (i/len(datasets), 0, 1 - i/len(datasets)))
+            ax[0].errorbar(voltages,currents,xerr = voltage_std, yerr = current_std, label = str("t="+str(i)), color = (i/len(datasets), 0, 1 - i/len(datasets)))
         else:
             pass
-            #plt.errorbar(voltages,currents,xerr = voltage_std, yerr = current_std, color = (i/len(datasets), 0, 1 - i/len(datasets)))
+            #ax[0].errorbar(voltages,currents,xerr = voltage_std, yerr = current_std, color = (i/len(datasets), 0, 1 - i/len(datasets)))
         i+=1
 
 #linear fit at steepest point to find threshold voltage
@@ -71,7 +74,7 @@ def plot_steepest_line(datasets):
                     max_derivative_index = i
                     gradient_err = gradient * np.sqrt((current_std[i] / currents[i])**2 + (voltage_std[i + 1] / voltages[i + 1])**2 + (voltage_std[i] / voltages[i])**2)
         f = lambda x : max_derivative * (x - voltages[max_derivative_index]) + currents[max_derivative_index]
-        plt.errorbar(voltages, list(map(f,(voltages))),fmt="--", color = 'red')
+        ax[0].errorbar(voltages, list(map(f,(voltages))),fmt="--", color = 'red')
 
         #error calculations
         x_intercept = (-currents[max_derivative_index] / max_derivative) + voltages[max_derivative_index]
@@ -112,7 +115,7 @@ def plot_derivative(datasets):
         print("offset:", offset)
         voltages = voltages[offset:-offset-1]
         print("voltages:", len(voltages), "derivative2:", len(derivative2))
-        plt.errorbar(voltages,scaled_derivative2,fmt="--", color = 'green')
+        ax[0].errorbar(voltages,scaled_derivative2,fmt="--", color = 'green')
         index_max_derivative2 = derivative2.index(max(derivative2))
         max_derivative2_voltage = voltages[index_max_derivative2]
         error = max((voltages[index_max_derivative2 + 1]) - max_derivative2_voltage, max_derivative2_voltage - voltages[index_max_derivative2 - 1])
@@ -138,7 +141,7 @@ def plot_derivative_thresholds_time(datasets, times=None, colour = "green", labe
         index_max_derivative2 = derivative2.index(max(derivative2))
         max_derivative2_voltages.append(voltages[index_max_derivative2])
         errors.append(max((voltages[index_max_derivative2 + 1]) - voltages[index_max_derivative2], voltages[index_max_derivative2] - voltages[index_max_derivative2 - 1]))
-    plt.errorbar(times, max_derivative2_voltages, yerr = errors, color = colour, label = label)
+    ax[0].errorbar(times, max_derivative2_voltages, yerr = errors, color = colour, label = label)
 
 def plot_steepest_threshold_time(datasets, times=None, colour = "red", label="steepest line method"):
     x_intercepts = []
@@ -165,7 +168,7 @@ def plot_steepest_threshold_time(datasets, times=None, colour = "red", label="st
         error = np.sqrt(0.005**2 + (currents[max_derivative_index] / max_derivative) * np.sqrt((current_std[max_derivative_index] / currents[max_derivative_index])**2 + (gradient_err / max_derivative)**2)**2 + voltage_std[max_derivative_index]**2)
         x_intercepts.append(x_intercept)
         errors.append(error)
-    plt.errorbar(times, x_intercepts, yerr = errors, color = colour, label=label)
+    ax[0].errorbar(times, x_intercepts, yerr = errors, color = colour, label=label)
     return [x_intercepts, errors]
 
 datasets = xml_to_datasets(data_file)
@@ -176,12 +179,12 @@ if data_file2 != None:
 if iv_graph:
     plot_iv(datasets)
     title = "Id vs Vg for a MOSFET"
-    plt.ylabel('Id')
-    plt.xlabel('Vg')
+    ax[0].ylabel('Id')
+    ax[0].xlabel('Vg')
 if steepest_line:
     plot_steepest_line(datasets)
-    plt.xlim(1.35, 2.85)
-    plt.ylim(0, 0.00015)
+    ax[0].xlim(1.35, 2.85)
+    ax[0].ylim(0, 0.00015)
 if derivative:
     plot_derivative(datasets)
 if dvt_over_time:
@@ -189,9 +192,9 @@ if dvt_over_time:
     if data_file2 != None:
         plot_derivative_thresholds_time(datasets2, times=range(len(datasets), len(datasets) + len(datasets2)),colour = "blue",label="2nd derivative method (no source)")
     title = "Threshold Voltage vs Time"
-    plt.xlim(left=0)
-    plt.ylabel('Vt')
-    plt.xlabel('Time hrs')
+    ax[0].xlim(left=0)
+    ax[0].ylabel('Vt')
+    ax[0].xlabel('Time hrs')
 if lvt_over_time:
     title = "Threshold Voltage vs Time"
     threshold_voltages = plot_steepest_threshold_time(datasets)
@@ -202,12 +205,20 @@ if lvt_over_time:
     if linear_fit:
         z = np.polyfit(times, threshold_voltages[0], 1)
         Y1p = np.poly1d(z)
-        plt.errorbar(times, Y1p(times), fmt = '--', color = "black", label = 'model')
+        ax[0].errorbar(times, Y1p(times), fmt = '--', color = "black", label = 'model')
         chi_2 = 0
         for i in range(len(threshold_voltages[0])):
             chi_2 += (threshold_voltages[0][i] - Y1p(times[i]))**2 /(threshold_voltages[1][i]**2)
+        #add residuals
+        Res_1s = np.array(threshold_voltages[0]) - np.array(Y1p(times))
+
+        #plot residuals
+        ax[1].plot(times, Res_1s/np.std(Res_1s), marker = '.', color = "red",linewidth = 0)
+        ax[1].plot(times, np.zeros(len(times)), color = "black", linestyle = "--")
+
         print("chi^2:", chi_2)
         print("Normalised chi^2:", chi_2 / (len(threshold_voltages[0]) - 2))
+        print("The prob of this occuring by chance is:", 1 - stats.chi2.cdf(chi_2, len(threshold_voltages[0]) - 2))
 
 #define file name
 if iv_graph:
@@ -217,7 +228,15 @@ if dvt_over_time or lvt_over_time:
 if data_file2 != None:
     file_name = "Graphs/" + data_file[5:-4] + "_VtT" + "_combined" + data_file[5:-4] + ".png"
 #making graph details
-plt.legend()
-plt.title(title)
+font = {'family': 'serif',
+        'color':  'black',
+        'weight': 'normal',
+        'size': 18,
+        }
+ax[1].set_ylabel("Normalised \nResiduals", fontdict = font)
+ax[1].set_xlabel('Time (hrs)', fontdict = font)
+ax[0].set_ylabel("Threshold Voltage (V)", fontdict = font)
+ax[0].legend(prop={'family': 'serif'}, edgecolor = "black", fancybox=False)
+ax[0].set_title(title)
 plt.savefig(fname = file_name)
 plt.show()
